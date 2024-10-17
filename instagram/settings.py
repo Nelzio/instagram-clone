b47e1d91e1849b69539d648142aa35a759c5b74f
Nelzio <nelziositoe@gmail.com>
2024-10-17 09:51:54 +0200

    Trocar local de media

--- instagram/settings.py
+++ instagram/settings.py
@@ -118,6 +118,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Media files (Uploaded by users)
+# https://docs.djangoproject.com/en/5.1/ref/settings/#media-root
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
